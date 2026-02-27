15cc9a19c38d75e4bd350a1963928b59a01fac5a
Ike <ike@Ikeui-MacBookAir.local>
2026-02-28 05:38:21 +0900

    네이버 Search Advisor 인증 태그

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
+        <meta name="naver-site-verification" content="c0f5bb30bfbc433725d6a1816dfa33c5fe6f555e" />
         <meta name="google-site-verification" content="UN72h2a-ElW3ik41MG-aquSIaDI005su-ulESwBOlBw" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&family=Gaegu:wght@400;700&display=swap"
